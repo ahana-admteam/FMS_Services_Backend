631f4d4f0b651c4621aaa826cccf49de1c37ee5d
Siddesh D R <siddesh.dr@ahanait.com>
2026-04-16 12:30:11 +0530

    gett all fms

--- src/controller/ahanaLibrary/getFmsTasks.controller.js
+++ src/controller/ahanaLibrary/getFmsTasks.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const getFmsTasks = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+let MongoClient = require('mongodb').MongoClient;
 const axios = require('axios');
 const moment = require('moment-timezone');
 const { fetchUserDetails } = require("../../../helpers/fetchuserDetails");
@@ -14,206 +14,56 @@ const FmsQA = require("../../models/fmsQA.model");
 
 //find ALL FMS Tasks FOR A USER 
 getFmsTasks.get('/findAllFmsTasksForUser', async (req, res) => {
+ try {
+    const token = req.headers.authorization;
 
-    let userDetails = await fetchUserDetails(req.headers.authorization);
-    let userName = userDetails.userName;
-    let userID = userDetails.userID;
-    let userEmail = userDetails.userEmail;
-
-    try {
-
-        infoLogger.log("info", `Username: ${userName} hit the api findAllFmsTasksForUser`)
-
-        const documents = await FmsTasks.find({ "fmsTaskDoer.employeeId": userID });
-
-        console.log("documents", documents);
-
-        res.json({
-            "message": [documents],
-            "status": 200
-        })
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        errorLogger.log("error" , `Username:${userName} failed to fetch all fms tasks due to ${error.message}`);
-        return res.status(500).json({ error: error.message });
-    }
-})
-
-
-//find ALL FMS Tasks FOR A USER THAT ARE DUE TODAY AND ALL OVERDUE TASKS
-getFmsTasks.get('/findAllFmsTasksForUserPendingOrOverdue', async (req, res) => {
-
-    let userDetails = await fetchUserDetails(req.headers.authorization);
-    let userName = userDetails.userName;
-    let userID = userDetails.userID;
-    let userEmail = userDetails.userEmail;
-
-    try {
-
-        infoLogger.log("info", `Username: ${userName} hit the api findAllFmsTasksForUserPendingOrOverdue`)
-
-        const pendingTasks = await FmsTasks.find({
-            fmsTaskStatus: 'PENDING',
-            'fmsTaskDoer.employeeId': userID
-        });
-
-        const currentTimeIST = moment().tz('Asia/Kolkata').format();
-
-        for (const task of pendingTasks) {
-
-            if (task.fmsTaskPlannedCompletionTime < currentTimeIST) {
-                await FmsTasks.updateOne(
-                    { fmsTaskId: task.fmsTaskId },
-                    { $set: { fmsTaskStatus: 'OVERDUE' } }
-                );
-            }
-        }
-
-    } catch (error) {
-        console.error('Error', error);
-        return res.status(500).json({ error: error.message });
+    if (!token) {
+      return res.status(401).json({ message: "Authorization header missing" });
     }
 
-    try {
-
-        const documents = await FmsTasks.find({
-            "fmsTaskDoer.employeeId": userID,
-            $or: [
-                { fmsTaskStatus: "PENDING" },
-                { fmsTaskStatus: "OVERDUE" }
-            ]
-        });
-
-        res.json({
-            "message": [documents],
-            "status": 200
-        })
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        errorLogger.log("error" , `Username:${userName} failed to fetch pending or overdue`);
-        return res.status(500).json({ error: error.message });
-    }
-})
-
-
-//ALL OVERDUE tasks
-getFmsTasks.get('/findAllFmsOverDueTasks', async (req, res) => {
-
-    let userDetails = await fetchUserDetails(req.headers.authorization);
-    let userName = userDetails.userName;
-
-    try {
-
-        const documents = await FmsTasks.find({ fmsTaskStatus: "OVERDUE" });
-
-        res.json({
-            "message": [documents],
-            "status": 200
-        })
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        return res.status(500).json({ error: error.message });
-    }
-})
-
-
-//ALL PENDING tasks
-getFmsTasks.get('/findAllFmsPendingTasks', async (req, res) => {
-
-    let userDetails = await fetchUserDetails(req.headers.authorization);
-    let userName = userDetails.userName;
-
-    try {
-
-        const documents = await FmsTasks.find({ fmsTaskStatus: "PENDING" });
-
-        res.json({
-            "message": [documents],
-            "status": 200
-        })
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        return res.status(500).json({ error: error.message });
-    }
-})
-
-
-//ALL OVERDUE tasks for user
-getFmsTasks.get('/findAllFmsOverdueTasksForUser', async (req, res) => {
-
-    let userDetails = await fetchUserDetails(req.headers.authorization);
-    let userName = userDetails.userName;
-    let userID = userDetails.userID;
-
-    try {
-
-        const documents = await FmsTasks.find({
-            fmsTaskStatus: "OVERDUE",
-            "fmsTaskDoer.employeeId": userID
-        });
-
-        res.json({
-            "message": [documents],
-            "status": 200
-        })
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        return res.status(500).json({ error: error.message });
-    }
-})
-
-
-//All Tasks for PC
-getFmsTasks.get('/findAllFmsOverdueTasksForPc', async (req, res) => {
-
-    let userDetails = await fetchUserDetails(req.headers.authorization);
-    let userName = userDetails.userName;
-
-    let fmsMasterIds = [];
-
-    try {
-
-        const documents = await FmsMaster.find({
-            "fmsProcess.processCoordinatorName": userName
-        });
-
-        fmsMasterIds = documents.map(doc => doc.fmsMasterId);
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        return res.status(500).json({ error: error.message });
-    }
-
-    let overDueTasksForPc;
-
-    try {
-
-        overDueTasksForPc = await FmsTasks.find({
-            fmsMasterId: { $in: fmsMasterIds },
-            fmsTaskStatus: "OVERDUE"
-        });
-
-    }
-    catch (error) {
-        console.error('Error', error);
-        return res.status(500).json({ error: error.message });
-    }
+    // ✅ Fetch actual user details from token
+    const userDetails = await fetchUserDetails(token);
+    const documents = await FmsTasks.find({ "fmsTaskDoer.empId": userDetails.result.emp_id });
 
     res.json({
-        "message": [overDueTasksForPc],
-        "status": 200
-    })
+      message: documents,
+      status: 200
+    });
+
+  } catch (error) {
+    console.error("Error in findSingleFms:", error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+//ALL PENDING tasks
+getFmsTasks.get('/finduserFmsTasks', async (req, res) => {
+    try {
+
+        const token = req.headers.authorization;
+        
+            if (!token) {
+              return res.status(401).json({ message: "Authorization header missing" });
+            }
+        
+            // ✅ Fetch actual user details from token
+            const userDetails = await fetchUserDetails(token);
+
+        const documents = await FmsTasks.find(
+            {"fmsTaskDoer.empId": userDetails.result.emp_id, fmsTaskStatus: { $in: ["PENDING", "COMPLETED"] }}
+        );
+
+        res.json({
+            "message": documents,
+            "status": 200
+        })
+
+    }
+    catch (error) {
+        console.error('Error', error);
+        return res.status(500).json({ error: error.message });
+    }
 })
+
 
 module.exports = getFmsTasks;
